05360ef71d52ab845277a7a318f9a37ee734233b
Cecilia Sanare <admin@cecilias.me>
2023-08-08 17:53:11 -0500

    ci: setup to run at 4 AM every day

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Refreshly } from '@refreshly/core';
+import { LogLevel, Logger, Refreshly } from '@refreshly/core';
 import { AWS } from '@refreshly/aws';
 import { GitHub } from '@refreshly/github';
+
+Logger.setLevel(LogLevel.SILLY);
 
 Refreshly(
   new AWS.Source({
